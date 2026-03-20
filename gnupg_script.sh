0034433887ca4809754771dd2163c3fa24970cb4
keylaim100 <keimbaun@uide.edu.ec>
2026-03-20 15:56:05 +0000

    my first commit

---
+++ gnupg_script.sh
@@ -0,0 +1,6 @@
+uname -a
+which gpg (para ver si esta instalado )
+gpg --version (ver version de gpg)
+gpg --full-generate-key (generar llaves)
+gpg --list-keys (llavero digital)
+gpg --armor --export (exportar la llave publica)
