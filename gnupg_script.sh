2d929029df4421701fd16442306a77405cd4bd35
keylaim100 <keimbaun@uide.edu.ec>
2026-03-23 14:46:36 +0000

    list secret keys

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,6 +1,14 @@
-uname -a
-which gpg (para ver si esta instalado )
-gpg --version (ver version de gpg)
-gpg --full-generate-key (generar llaves)
-gpg --list-keys (llavero digital)
-gpg --armor --export (exportar la llave publica)
+uname -a #conocer el sistema instalado
+which gpg #para ver si esta instalado 
+gpg --version #ver version de gpg
+gpg --full-generate-key #generar llaves
+gpg --list-keys #llavero digital
+gpg --armor --export #exportar la llave publica
+gpg --list-keys #lista todas las llaves
+#para listar las llaves privadas
+gpg --list-secret-keys --keyid-format=long 
+
+
+
+
+
