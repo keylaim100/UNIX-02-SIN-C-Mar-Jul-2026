4c92fcb1f3c5633cd1116f450adbbc2eb95c6c3b
keylaim100 <keimbaun@uide.edu.ec>
2026-03-23 14:49:26 +0000

    export secret key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,9 +5,11 @@ gpg --full-generate-key #generar llaves
 gpg --list-keys #llavero digital
 gpg --armor --export #exportar la llave publica
 gpg --list-keys #lista todas las llaves
+
 #para listar las llaves privadas
 gpg --list-secret-keys --keyid-format=long 
-
+#exporta la llave privada 
+gpg --armor --export-secret-keys 08F019E40AC3E0AE
 
 
 
