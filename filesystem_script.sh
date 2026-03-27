ae018fe61d8c57e32978380f511be4ce2636de30
keylaim100 <keimbaun@uide.edu.ec>
2026-03-27 15:21:48 +0000

    wip: coding in C

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -8,3 +8,15 @@ cd /proc
 ls
 # shows cpu information
 cat cpuinfo
+# helps us to move to the root directory
+cd /
+#list the files and the directories in the current directory
+ls
+# help to move to the bin directory
+cd /bin
+#list the files and the directories
+ls
+#To move to the directory where we have our code
+cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/
+#compiles the C file and creates an executable file named saludo_bin
+gcc saludo.c -o saludo_bin
