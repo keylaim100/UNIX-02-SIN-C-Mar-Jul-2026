ba7c51ba9574a9b2165d9ebe28b45c328998160f
keylaim100 <keimbaun@uide.edu.ec>
2026-03-30 15:27:11 +0000

    Organization of the disks

--- scriptt.sh
+++ scriptt.sh
@@ -6,3 +6,8 @@ sudo apt upgrade
 sudo apt install
 # Install the NumPy library in Python to make calculations
 pip install numpy
+# full report and organization of the disks
+sudo parted -l && echo -e "\n--- \n" && lsblk -f && echo -e "\n --- \n"
+parted -l # muestra las particones de los discos
+lsblk -f # muestra la estructura de los discos, UUIDs (identificador unico de la particion) y espacios disponibles
+loop # it creates a loop devide
