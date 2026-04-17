b45d9705ca9f35f78ec95fa0985fdbff4875ff23
keylaim100 <keimbaun@uide.edu.ec>
2026-04-17 14:41:20 +0000

    wip: manual page

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -18,7 +18,7 @@ LICENSE     darwin_doc_cifrado_y_firmado.txt  doc_cifrado.txt                   
 LICENSE     darwin_doc_cifrado_y_firmado.txt  doc_cifrado.txt                            doc_no_cifrado_firmado_binario.txt
 
 # list all permissions, users, hidden files, etc
-# the three do the same as me
+# The three do the same thing
 ls -l -a -h
 ls -l -ah
 ls -lah
@@ -29,4 +29,8 @@ mkdir -rf
 mkdir -- -rf
 #to delete the directory
 rmdir -- -rf
-
+# Manual
+# quick summary version
+ls --help
+# For COMPLETE NAVIGATION MENU: /(TEXT TO SEARCH) "n" FORWARD "N" BACK, q TO EXIT
+man ls
