566abb8d2161801441be1f0f20c96ebf3531f3dd
keylaim100 <keimbaun@uide.edu.ec>
2026-04-20 14:30:20 +0000

    wip: sudo permission denied

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -4,18 +4,18 @@ ls -a
 # this is the long version
 ls --all
 #result of both
-@keylaim100 ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -a
-.           README.md                         darwin_doc_no_cifrado.txt                  doc_cifrado_y_firmado.txt                        firma_separada_doc_no_cifrado.sig
-..          amigo_doc_cifrado.txt             darwin_doc_no_cifrado_firmado.txt          doc_cifrado_y_firmado_descifrado_y_validado.txt  gnupg_script.sh
-.git        amigo_llave_publica.asc           darwin_doc_no_cifrado_firmado_binario.txt  doc_no_cifrado.txt                               mi_llave_publica.asc
-.gitignore  boot_exploration.sh               darwin_firma_separada_doc_no_cifrado.sig   doc_no_cifrado_firmado.txt
-LICENSE     darwin_doc_cifrado_y_firmado.txt  doc_cifrado.txt                            doc_no_cifrado_firmado_binario.txt
-@keylaim100 ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls --all
-.           README.md                         darwin_doc_no_cifrado.txt                  doc_cifrado_y_firmado.txt                        firma_separada_doc_no_cifrado.sig
-..          amigo_doc_cifrado.txt             darwin_doc_no_cifrado_firmado.txt          doc_cifrado_y_firmado_descifrado_y_validado.txt  gnupg_script.sh
-.git        amigo_llave_publica.asc           darwin_doc_no_cifrado_firmado_binario.txt  doc_no_cifrado.txt                               mi_llave_publica.asc
-.gitignore  boot_exploration.sh               darwin_firma_separada_doc_no_cifrado.sig   doc_no_cifrado_firmado.txt
-LICENSE     darwin_doc_cifrado_y_firmado.txt  doc_cifrado.txt                            doc_no_cifrado_firmado_binario.txt
+#@keylaim100 ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -a
+#.           README.md                         darwin_doc_no_cifrado.txt                  doc_cifrado_y_firmado.txt                        firma_separada_doc_no_cifrado.sig
+#..          amigo_doc_cifrado.txt             darwin_doc_no_cifrado_firmado.txt          doc_cifrado_y_firmado_descifrado_y_validado.txt  gnupg_script.sh
+#.git        amigo_llave_publica.asc           darwin_doc_no_cifrado_firmado_binario.txt  doc_no_cifrado.txt                               mi_llave_publica.asc
+#.gitignore  boot_exploration.sh               darwin_firma_separada_doc_no_cifrado.sig   doc_no_cifrado_firmado.txt
+#LICENSE     darwin_doc_cifrado_y_firmado.txt  doc_cifrado.txt                            doc_no_cifrado_firmado_binario.txt
+#@keylaim100 ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls --all
+#.           README.md                         darwin_doc_no_cifrado.txt                  doc_cifrado_y_firmado.txt                        firma_separada_doc_no_cifrado.sig
+#..          amigo_doc_cifrado.txt             darwin_doc_no_cifrado_firmado.txt          doc_cifrado_y_firmado_descifrado_y_validado.txt  gnupg_script.sh
+#.git        amigo_llave_publica.asc           darwin_doc_no_cifrado_firmado_binario.txt  doc_no_cifrado.txt                               mi_llave_publica.asc
+#.gitignore  boot_exploration.sh               darwin_firma_separada_doc_no_cifrado.sig   doc_no_cifrado_firmado.txt
+#LICENSE     darwin_doc_cifrado_y_firmado.txt  doc_cifrado.txt                            doc_no_cifrado_firmado_binario.txt
 
 # list all permissions, users, hidden files, etc
 # The three do the same thing
@@ -61,5 +61,10 @@ chmod u+rw,go-rwx privado
 # - to remove
 # + to add
 # , to add operation
+
+#super user, system administrator user, only runs in the command below
+sudo #(super user do)
+sudo echo "hola" > /etc/archivo_protegido # It does not give permission because sudo is executed on the command that is next to it
+
 
 
