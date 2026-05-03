cd8228e1048d72c277e3f3af9939e9c0de0c3ab2
keylaim100 <keimbaun@uide.edu.ec>
2026-05-03 17:42:02 +0000

    wip: list inodes

--- script_boot.sh
+++ script_boot.sh
@@ -2,7 +2,10 @@
 cd calculadora/
 #absolute path
 cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora
-
+#l=list a=all i=inode
+ls -lai
+#the same but in separate way
+ls -l -a -i
 # Update the repositories and install the necessary tools (compilers, libraries, and emulator) to compile and test a Linux kernel from scratch
 sudo apt update
 sudo apt install -y git vim make gcc libncurses-dev flex bison bc \
