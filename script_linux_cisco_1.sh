56c5b52c666b90cb1bc9bd246a6ef7824b73d0dd
keylaim100 <keimbaun@uide.edu.ec>
2026-05-03 20:27:27 +0000

    wip: ls command with different arguments

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -5,3 +5,11 @@ ls
 aptitude moo
 #result
 #There are no Easter Eggs in this program
+
+#displays directory contents in a long-listing format, showing detailed information such as file permissions, number of links, owner, group, file size, and the last modification date
+ls -l
+
+#lists the contents of the current directory in reverse alphabetical order
+ls -r
+#displays directory contents in a long-listing format while reversing the alphabetical sorting order
+ls -lr 
