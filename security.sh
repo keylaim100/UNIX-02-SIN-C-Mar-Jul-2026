0329fb7a3f06b4d4b3f99c4d41509ba4fb3ec32b
keylaim100 <keimbaun@uide.edu.ec>
2026-05-08 14:35:31 +0000

    wip: id, groups and cat

---
+++ security.sh
@@ -0,0 +1,8 @@
+# displays the real and effective user and group IDs for the current or specified user
+id 
+#displays the names of the groups that the current user
+groups
+# displays a colon-separated text file that contains essential information about the user accounts
+cat /etc/passwd
+# retrieves the first 10 lines of the system's user database
+cat /etc/passwd | head -10
