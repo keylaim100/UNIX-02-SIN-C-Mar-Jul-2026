96c065ef67441bfde5bde86858507fd9cfef703f
keylaim100 <keimbaun@uide.edu.ec>
2026-05-29 15:37:34 +0000

    wip: group desarrolladres

--- security2.zsh
+++ security2.zsh
@@ -14,3 +14,25 @@ echo "Grupo actual: $(id -gn)"
 #create a file before newgrp
 touch ~/antes_de_newgrp.txt
 ls -la ~/antes_de_newgrp.txt
+
+#create the group for testing
+groupadd desarrolladores
+
+#install utilities required by newgrp
+apt install util-linux-extra
+
+#change root's default shell to bash
+usermod -s /bin/bash root
+
+#verify the shell change
+grep root /etc/passwd
+
+#switch to the desarrolladores group
+newgrp desarrolladores
+
+#verify the current group
+id -gn
+
+#a file is created and it is verified that the group is desarrolladores
+touch ~/despues_de_newgrp.txt
+ls -la ~/despues_de_newgrp
