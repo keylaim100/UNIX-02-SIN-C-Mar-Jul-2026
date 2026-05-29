7e962411c7480f214f1ed5427ccd60d312aa077e
keylaim100 <keimbaun@uide.edu.ec>
2026-05-29 15:07:59 +0000

    wip: newgrp

--- security2.zsh
+++ security2.zsh
@@ -6,3 +6,11 @@ id -gn
 touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 #the group is the user's primary group 
+
+#see all available groups
+groups 
+
+echo "Grupo actual: $(id -gn)"
+#create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
