55e5d898d8a93e812d818f1d890ff8eea14a2549
keylaim100 <keimbaun@uide.edu.ec>
2026-06-01 15:05:10 +0000

    wip: shebang

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+#!/usr/bin/env bash
 bash --version
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+ps -ef
+df --human-readable
+#bash -x blackhatbash1.sh
+#bash -r blackhatbash1.sh 
