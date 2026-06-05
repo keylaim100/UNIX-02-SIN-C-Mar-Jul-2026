04f5b6523e95520be989eea0462887910cf6efdf
keylaim100 <keimbaun@uide.edu.ec>
2026-06-05 15:38:09 +0000

    wip: Redirection Operators

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,14 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "el comando lzl falló"
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Bye World!" > output.txt
+cat output.txt
+echo "Goodbye!" >> output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt                
