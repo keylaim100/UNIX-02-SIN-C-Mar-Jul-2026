382504918765c5d5fbabe3f636d187f379310499
keylaim100 <keimbaun@uide.edu.ec>
2026-06-05 15:43:17 +0000

    wip: error.txt

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -13,4 +13,11 @@ ls -l / &> stdout_and_stderr.txt
 cat stdout_and_stderr.txt
 ls -l / 1> stdout.txt 2> stderr.txt
 cat stdout.txt
-cat stderr.txt                
+cat stderr.txt      
+lzl 2> error.txt     
+cat error.txt
+cat < output.txt
+cat << EOF
+Black Hat Bash
+by No Starch Press
+EOF
