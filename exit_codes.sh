775218960ad09a5ad74a03d633877f9e5bbb77e7
keylaim100 <keimbaun@uide.edu.ec>
2026-06-08 15:43:24 +0000

    wip: Setting a Script’s Exit Code

--- exit_codes.sh
+++ exit_codes.sh
@@ -4,3 +4,7 @@ ls -l > /dev/null
 echo "The exit code of the ls command was: $?"
 lzl 2> /dev/null
 echo "The exit code of the non-existing lzl command was: $?"
+
+# Sets the exit code of the script to be 223
+echo "Exiting with exit code: 223"
+exit 223
