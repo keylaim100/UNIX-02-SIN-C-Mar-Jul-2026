33b5874a619faee002794d035a697e9e7702bab5
keylaim100 <keimbaun@uide.edu.ec>
2026-06-20 13:48:44 -0500

    awk

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -5,3 +5,14 @@ ps | grep TTY
 ps | grep tty
 ps | grep -i tty
 grep -v "35.237.4.214" log.txt
+
+grep -o "35.237.4.214" log.txt
+awk '{print $1}' log.txt
+awk '{print $3}' log.txt
+awk '{print $1,$2,$3}' log.txt
+awk '{print $1,$NF}' log.txt
+
+touch test.csv
+echo -e "hola1,chao1\nhola2,chao2\nhola3,chao3" >> test.csv
+awk -F',' '{print $1}' test.csv
+
