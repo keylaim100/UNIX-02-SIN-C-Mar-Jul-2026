dce498a21f7ae90866288549415f7e9a0baefda9
keylaim100 <keimbaun@uide.edu.ec>
2026-06-20 14:00:45 -0500

    editing with sed 2

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -30,3 +30,9 @@ sed 's/Mozilla/Godzilla/g' log.txt
 grep "Godzilla" log.txt
 sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
 
+sed 's/ //g' log.txt
+sed '1d' log.txt
+sed '$d' log.txt
+sed '5,7d' log.txt
+sed -n '2,15 p' log.txt
+sed -i '1d' log.txt
